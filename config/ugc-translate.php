2cfda4b4b919f38a2780dc9a1926f5230e2db166
Rich Porter <rich.porter@carandclassic.com>
2022-10-31 15:02:08 +0000

    updating config file

--- config/ugc-translate.php
+++ config/ugc-translate.php
@@ -1,6 +1,7 @@
 <?php
 
-// config for RpWebDevelopment/LaravelUgcTranslate
 return [
-
+    'auth-token' => env('DEEPL_AUTH_TOKEN', ''),
+    'auto-translate-disabled' => env('DEEPL_AUTO_DISABLED', false),
+    'default-lang' => 'en_US',
 ];
